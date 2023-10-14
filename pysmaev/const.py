31ed267eb7ab23e1a3824c6307ba8b105274940f
Andre Lengwenus <alengwenus@gmail.com>
2023-10-14 12:10:30 +0000

    Add enums for parameters and measurements

--- pysmaev/const.py
+++ pysmaev/const.py
@@ -1,4 +1,5 @@
 """Constants for pysmaev."""
+from enum import Enum, StrEnum
 
 URL_TOKEN = "/api/v1/token"
 URL_MEASUREMENTS = "/api/v1/measurements/live"
@@ -13,3 +14,35 @@ HEADER_CONTENT_TYPE_JSON = "application/json"
 
 REQUEST_TIMEOUT = 15
 TOKEN_TIMEOUT = 3000
+
+
+class SmaEvChargerMeasurements(Enum):
+    """EV Charger Measurements."""
+
+    # Position of rotary switch
+    SMART_CHARGING = 4950
+    BOOST_CHARGING = 4718
+
+    # Charging session status
+    NOT_CONNECTED = 200111
+    SLEEP_MODE = 200112
+    ACTIVE_MODE = 200113
+
+
+class SmaEvChargerParameters(StrEnum):
+    """EV Charger Parameters."""
+
+    # Operating mode of charge session
+    BOOST_CHARGING = "4718"
+    OPTIMIZED_CHARGING = "4719"
+    SETPOINT_CHARGING = "4720"
+    CHARGE_STOP = "4721"
+
+    # Manual charging release
+    CHARGING_LOCK = "5171"
+    CHARGING_RELEASE = "5172"
+
+    # Light brightness
+    LED_LOW = "5191"
+    LED_AVERAGE = "5190"
+    LED_HIGH = "5173"
